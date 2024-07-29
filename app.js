c00d4e0e2e6117f63f6c02804060384ec536efa8
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-07-29 14:58:47 -0400

    Updating Profile Page Post Route 70

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/login', async (req, res) => {
             return res.status(400).json({ message: 'Invalid Credentials '});
         }
         const token = jwt.sign({ userId: user.id }, process.env.DB_SECRET, { expiresIn: '1h' });
-        res.cookie('token', token, { httpOnly: true });
+        res.cookie('token', `Bearer ${token}`, { httpOnly: true });
         res.redirect('/Html/profile.html');
     } catch (error) {
         console.error('Error logging in:', error);
