2aed23bed65863ec63593600750a907d7073d532
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-07-30 18:17:09 -0400

    Updating Success and Error Messages 03

--- app.js
+++ app.js
@@ -224,8 +224,7 @@ app.get('/deleteuser', verifyToken, async (req, res) => {
 
 // Middleware to verify JWT token
 function verifyToken(req, res, next) {
-    const token = req.cookies.token
-    console.log(token);
+    const token = req.header('Authorization');
     if (!token) {
         return res.status(401).json({ message: 'Access Denied'});
     } try {
