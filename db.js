5bc6045af62308998215cdf3390b04c1394a9750
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-13 21:22:18 -0400

    updating 07

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize({
     database: "TravelApp",
     username: "Cyrus",
-    password: process.env.DB_PW,
+    password: "Eternal11!",
     host: "travel-application-users.czqk6e0s0vr8.us-east-2.rds.amazonaws.com",
     port: 5432,
     dialect: "postgres",
