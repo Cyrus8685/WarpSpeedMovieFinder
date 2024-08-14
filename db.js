140f0879ce6eed2dafa87c4054aad3d64e4eebc6
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-13 23:17:30 -0400

    updating MongoDB 12

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { mongoose, Collection } = require('mongoose');
 
-mongoose.connect(process.env.DB_URL), {
+mongoose.connect(process.env.DB_URL, {
   useUnifiedTopology: "true"
-})
+});
 
 module.exports = mongoose;
